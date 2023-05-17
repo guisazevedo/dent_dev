f71dfd8dc8bb53a7002110d2474107250c45a829
Guilherme de Azevedo <guisazevedo@gmail.com>
2023-05-18 01:45:18 +0200

    implement log-in

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import Carousel from "./02_block/Carousel";
 import Planos from "./02_block/Planos";
 import FAQ from "./02_block/FAQ";
 import Footer from "./02_block/Footer";
+import Signin from "./02_block/Signin";
 
 function App() {
   return (
-    <div className="m-5">
+    <div className="bg-light">
       <Navbar />
+      <div className="b-example-divider"></div>
       <Carousel />
-      <Planos />
+      <div className="b-example-divider"></div>
       <FAQ />
+      <div className="b-example-divider"></div>
+      <Planos />
       {/* <Footer /> */}
+      <Signin />
     </div>
   );
 }
