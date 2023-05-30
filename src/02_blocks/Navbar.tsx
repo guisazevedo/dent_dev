a05043b559ed7f4c64c4b7e95ed912c451981796
Guilherme de Azevedo <guisazevedo@gmail.com>
2023-05-30 14:29:09 +0200

    loops navbar

--- src/02_blocks/Navbar.tsx
+++ src/02_blocks/Navbar.tsx
@@ -3,6 +3,19 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { NavHashLink } from "react-router-hash-link";
 
 const Navbar = () => {
+  const menu_items = [
+    ["Principal", "/#carousel"],
+    ["Video", "/#video"],
+    ["FAQ", "/#faq"],
+    ["Contato", "/#contato"],
+    ["Planos", "/#planos"],
+  ];
+
+  const menu_logs = [
+    ["Log-in", "-primary", "/log-in"],
+    ["Sign-up", "-outline-dark", "/sign-up"],
+  ];
+
   return (
     <nav className="navbar navbar-expand-lg bg-light fixed-top p-2">
       <div className="container-fluid">
@@ -23,43 +36,23 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarColor02">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavHashLink className="nav-link text-dark ms-3" to="/#carousel">
-                Principal
-              </NavHashLink>
-            </li>
-            <li className="nav-item">
-              <NavHashLink className="nav-link text-dark ms-3" to="/#video">
-                Video
-              </NavHashLink>
-            </li>
-            <li className="nav-item">
-              <NavHashLink className="nav-link text-dark ms-3" to="/#faq">
-                FAQ
-              </NavHashLink>
-            </li>
-            <li className="nav-item">
-              <NavHashLink className="nav-link text-dark ms-3" to="/#contato">
-                Contato
-              </NavHashLink>
-            </li>
-            <li className="nav-item">
-              <NavHashLink className="nav-link text-dark ms-3" to="/#planos">
-                Planos
-              </NavHashLink>
-            </li>
+            {menu_items.map((item) => (
+              <li className="nav-item">
+                <NavHashLink className="nav-link text-dark ms-3" to={item[1]}>
+                  {item[0]}
+                </NavHashLink>
+              </li>
+            ))}
           </ul>
+
           <div className="d-flex">
-            <NavHashLink className="nav-link" to="/log-in">
-              <button className="btn btn-primary me-1" type="button">
-                Log-in
-              </button>
-            </NavHashLink>
-            <NavHashLink className="nav-link" to="/sign-up">
-              <button className="btn btn-outline-dark ms-1" type="button">
-                Sign-up
-              </button>
-            </NavHashLink>
+            {menu_logs.map((item) => (
+              <NavHashLink className="nav-link" to={item[2]}>
+                <button className={"btn btn" + item[1] + " mx-1"} type="button">
+                  {item[0]}
+                </button>
+              </NavHashLink>
+            ))}
           </div>
         </div>
       </div>
