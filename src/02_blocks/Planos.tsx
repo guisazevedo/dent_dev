bf9fe554aa2b25a092e48e57006dceea262ef4a7
Guilherme de Azevedo <guisazevedo@gmail.com>
2023-05-30 22:43:46 +0200

    xxx

--- src/02_blocks/Planos.tsx
+++ src/02_blocks/Planos.tsx
@@ -41,7 +41,7 @@ const Planos = () => {
   ];
 
   return (
-    <main className="pt-5 px-5 bg-light" id="planos">
+    <main className="pt-5 bg-light" id="planos">
       <div className="p-5 mx-auto">
         <div className="pricing-header p-5 pb-md-4 mx-auto text-center mb-5">
           <h1 className="display-4 fw-normal mb-3">Planos</h1>
